908fbdd38d3eb3f47bb0259bbb55c2bfc7f95741
shrivanshu <156309632+shrivanshu@users.noreply.github.com>
2025-01-10 15:42:24 +0530

    added employees detail

--- my-react-app/src/Employees.jsx
+++ my-react-app/src/Employees.jsx
@@ -1,22 +1,32 @@
 import React from "react";
 import "./Employees.css"
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 const Employees = () => {
   const employeeList = [
-    { id: 1, name: "John Doe", role: "Software Engineer" },
-    { id: 2, name: "Jane Smith", role: "HR Manager" }
-  ];
+    { id: 1, name: "Shrivanshu Dubey", role: "Software Engineer" },
+    { id: 2, name: "Sneha Chouhan", role: "Software Engineer" },
+    { id: 3, name: "Sourabh Pandey", role: "Software Engineer" }
+  ]
 
+  
   return (
-    <div>
-      <h2>Employee List</h2>
-      <div className="employee-list">
-        {employeeList.map((employee) => (
-          <div key={employee.id} className="employee-details">
-            <p>ID: {employee.id}</p>
-            <p>Name: {employee.name}</p>
-            <p>Role: {employee.role}</p>
+    <div className="app">
+      <Navbar />
+      <div className="main-content">
+        <Sidebar />
+        <div className="employee-container">
+          <h2>Employee List</h2>
+          <div className="employee-list">
+            {employeeList.map((employee) => (
+              <div key={employee.id} className="employee-details">
+                <p>ID: {employee.id}</p>
+                <p>Name: {employee.name}</p>
+                <p>Role: {employee.role}</p>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
