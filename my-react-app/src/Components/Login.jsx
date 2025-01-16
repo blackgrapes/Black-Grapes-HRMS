cdd76956f0eb07d700c474509b3e217aab0754ab
Sneha chouhan <93982861+Asteraein@users.noreply.github.com>
2025-01-16 14:38:05 +0530

    employeeLogin css done

--- my-react-app/src/Components/Login.jsx
+++ my-react-app/src/Components/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
         <div className="logo-section d-flex flex-column align-items-center justify-content-center">
           <img
             src="./public/asset/logo.png"
-            // Replace with the actual path to your logo
+               // Replace with the actual path to your logo
             alt="Black Grapes Softech Logo"
             className="logo"
           />
