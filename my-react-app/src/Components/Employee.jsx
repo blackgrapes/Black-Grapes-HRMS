0884776322cd1351761f86a1e1a09ec1f7966ca8
0sourabh <sourabhsompandey@gmail.com>
2025-01-18 14:19:16 +0530

    upadate employeee

--- my-react-app/src/Components/Employee.jsx
+++ my-react-app/src/Components/Employee.jsx
@@ -1,183 +1,87 @@
 import axios from "axios";
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-const AddEmployee = () => {
-  const [employee, setEmployee] = useState({
-    name: "",
-    email: "",
-    password: "",
-    salary: "",
-    address: "",
-    phone: "",
-    designation: "",
-    image: "",
-  });
+const Employee = () => {
+  const [employee, setEmployee] = useState([]);
+  const navigate = useNavigate()
 
-  const [errorMessage, setErrorMessage] = useState("");
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Simple validation
-    if (!employee.name || !employee.email || !employee.password || !employee.salary || !employee.phone || !employee.designation || !employee.image) {
-      setErrorMessage("Please fill all fields.");
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append("name", employee.name);
-    formData.append("email", employee.email);
-    formData.append("password", employee.password);
-    formData.append("address", employee.address);
-    formData.append("salary", employee.salary);
-    formData.append("phone", employee.phone);
-    formData.append("designation", employee.designation);
-    formData.append("image", employee.image);
-
+  useEffect(() => {
     axios
-      .post("http://localhost:3000/auth/add_employee", formData)
+      .get("http://localhost:3000/auth/employee")
       .then((result) => {
         if (result.data.Status) {
-          navigate("/dashboard/employee");
+          setEmployee(result.data.Result);
         } else {
           alert(result.data.Error);
         }
       })
       .catch((err) => console.log(err));
-  };
-
+  }, []);
+  const handleDelete = (id) => {
+    axios.delete('http://localhost:3000/auth/delete_employee/'+id)
+    .then(result => {
+        if(result.data.Status) {
+            window.location.reload()
+        } else {
+            alert(result.data.Error)
+        }
+    })
+  } 
   return (
-    <div className="d-flex justify-content-center align-items-center mt-3">
-      <div className="p-3 rounded w-50 border">
-        <h3 className="text-center">Add Employee</h3>
-        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-        <form className="row g-1" onSubmit={handleSubmit}>
-          <div className="col-12">
-            <label htmlFor="inputName" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              className="form-control rounded-0"
-              id="inputName"
-              placeholder="Enter Name"
-              onChange={(e) =>
-                setEmployee({ ...employee, name: e.target.value })
-              }
-            />
-          </div>
-          <div className="col-12">
-            <label htmlFor="inputEmail4" className="form-label">
-              Email
-            </label>
-            <input
-              type="email"
-              className="form-control rounded-0"
-              id="inputEmail4"
-              placeholder="Enter Email"
-              autoComplete="off"
-              onChange={(e) =>
-                setEmployee({ ...employee, email: e.target.value })
-              }
-            />
-          </div>
-          <div className="col-12">
-            <label htmlFor="inputPassword4" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control rounded-0"
-              id="inputPassword4"
-              placeholder="Enter Password"
-              onChange={(e) =>
-                setEmployee({ ...employee, password: e.target.value })
-              }
-            />
-          </div>
-          <div className="col-12">
-            <label htmlFor="inputPhone" className="form-label">
-              Phone Number
-            </label>
-            <input
-              type="text"
-              className="form-control rounded-0"
-              id="inputPhone"
-              placeholder="Enter Phone Number"
-              onChange={(e) =>
-                setEmployee({ ...employee, phone: e.target.value })
-              }
-            />
-          </div>
-          <div className="col-12">
-            <label htmlFor="inputDesignation" className="form-label">
-              Designation
-            </label>
-            <input
-              type="text"
-              className="form-control rounded-0"
-              id="inputDesignation"
-              placeholder="Enter Designation"
-              onChange={(e) =>
-                setEmployee({ ...employee, designation: e.target.value })
-              }
-            />
-          </div>
-          <div className="col-12">
-            <label htmlFor="inputSalary" className="form-label">
-              Salary
-            </label>
-            <input
-              type="text"
-              className="form-control rounded-0"
-              id="inputSalary"
-              placeholder="Enter Salary"
-              autoComplete="off"
-              onChange={(e) =>
-                setEmployee({ ...employee, salary: e.target.value })
-              }
-            />
-          </div>
-          <div className="col-12">
-            <label htmlFor="inputAddress" className="form-label">
-              Address
-            </label>
-            <input
-              type="text"
-              className="form-control rounded-0"
-              id="inputAddress"
-              placeholder="1234 Main St"
-              autoComplete="off"
-              onChange={(e) =>
-                setEmployee({ ...employee, address: e.target.value })
-              }
-            />
-          </div>
-          <div className="col-12 mb-3">
-            <label className="form-label" htmlFor="inputGroupFile01">
-              Select Image
-            </label>
-            <input
-              type="file"
-              className="form-control rounded-0"
-              id="inputGroupFile01"
-              name="image"
-              onChange={(e) =>
-                setEmployee({ ...employee, image: e.target.files[0] })
-              }
-            />
-          </div>
-          <div className="col-12">
-            <button type="submit" className="btn btn-primary w-100">
-              Add Employee
-            </button>
-          </div>
-        </form>
+    <div className="px-5 mt-3">
+      <div className="d-flex justify-content-center">
+        <h3>Employee List</h3>
+      </div>
+      <Link to="/dashboard/add_employee" className="btn btn-success">
+        Add Employee
+      </Link>
+      <div className="mt-3">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Salary</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employee.map((e) => (
+              <tr>
+                <td>{e.name}</td>
+                <td>
+                  <img
+                    src={`http://localhost:3000/Images/` + e.image}
+                    className="employee_image"
+                  />
+                </td>
+                <td>{e.email}</td>
+                <td>{e.address}</td>
+                <td>{e.salary}</td>
+                <td>
+                  <Link
+                    to={`/dashboard/edit_employee/` + e.id}
+                    className="btn btn-info btn-sm me-2"
+                  >
+                    Edit
+                  </Link>
+                  <button
+                    className="btn btn-warning btn-sm"
+                    onClick={() => handleDelete(e.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
 
-export default AddEmployee;
+export default Employee;
