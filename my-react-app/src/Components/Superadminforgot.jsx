38320e1dd5f6c1b6af8129288a8e4d5f2c624720
0sourabh <sourabhsompandey@gmail.com>
2025-02-11 17:16:43 +0530

    fix

--- my-react-app/src/Components/Superadminforgot.jsx
+++ my-react-app/src/Components/Superadminforgot.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './SuperadminForgot.css';
+import './Superadminforgot.css';
 
 const SuperadminForgot = () => {
   const [email, setEmail] = useState('');
